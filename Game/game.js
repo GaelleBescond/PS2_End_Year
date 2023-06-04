b31d370faddbd5800a28ca0c1226ca7fef4cb0bd
GaelleBescond <118370404+GaelleBescond@users.noreply.github.com>
2023-06-04 18:12:05 +0200

    Push rendu?

---
+++ Game/game.js
@@ -0,0 +1,62 @@
+//global game parameters
+import PreloadScene from './preload.js';
+import LevelTemplate from './scenes_templates/level_template.js';
+import Interface from './UI/interface.js';
+import MainMenu from './UI/mainMenu.js';
+import Settings from './UI/settings.js';
+import Key_Bindings from './UI/keyBindings.js';
+
+import Mission01_scene01 from './scenes/mission01_scene01.js';
+import Mission01_scene02 from './scenes/mission01_scene02.js';
+import Mission01_scene03 from './scenes/mission01_scene03.js';
+import Mission01_scene04 from './scenes/mission01_scene04.js';
+import Mission01_scene05 from './scenes/mission01_scene05.js';
+import Mission01_scene06 from './scenes/mission01_scene06.js';
+const WIDTH = 1920;
+const HEIGHT = 1080;
+const ZOOM_FACTOR = 2;
+
+const SHARED_CONFIG = {
+  width: WIDTH,
+  height: HEIGHT,
+  zoomFactor: ZOOM_FACTOR,
+  leftTopCorner: {
+    x: (WIDTH - (WIDTH / ZOOM_FACTOR)) / 2,
+    y: (HEIGHT - (HEIGHT / ZOOM_FACTOR)) / 2
+  }
+}
+
+const Scenes = [
+  PreloadScene,
+  Interface,
+  MainMenu,
+  Settings,
+  Key_Bindings,
+  LevelTemplate,
+  Mission01_scene01,
+  Mission01_scene02,
+  Mission01_scene03,
+  Mission01_scene04,
+  Mission01_scene05,
+  Mission01_scene06,
+
+];
+const createScene = Scene => new Scene(SHARED_CONFIG) //A voir
+const initScenes = () => Scenes.map(createScene)
+
+const config = {
+  type: Phaser.AUTO,
+  ...SHARED_CONFIG,
+  physics: {
+    arcade: {
+      pixelArt: true,
+      tileBias: 128
+    },
+    fps: {
+      target: 60
+    }
+  },
+  scene: initScenes()
+};
+
+new Phaser.Game(config);
---
+++ Game/preload.js
@@ -0,0 +1,135 @@
+class Preload extends Phaser.Scene {
+
+    constructor() {
+        super("PreloadScene");
+    }
+
+    preload() {
+        //Images
+        this.load.image('enemy_soldier', 'assets/enemy_soldier.png');
+        this.load.image('enemy_tank', 'assets/enemy_tank.png');
+        this.load.image('enemy_turret', 'assets/enemy_turret.png');
+        this.load.image('crosshair', 'assets/crosshair.png');
+        this.load.image('checkpoint', 'assets/checkpoint.png');
+        this.load.image('particle_rifle', 'assets/particle_rifle.png');
+        this.load.image('particle_sniper', 'assets/particle_sniper.png');
+        this.load.image('particle_mortar', 'assets/particle_mortar.png');
+
+        this.load.image('practice_target', 'assets/practice_target.png');
+        this.load.image('door', 'assets/door.png');
+        this.load.image('bullet_rifle', 'assets/bullet_rifle.png');
+        this.load.image('bullet_sniper', 'assets/bullet_sniper.png');
+        this.load.image('bullet_mortar', 'assets/bullet_mortar.png');
+        this.load.image('platform', 'assets/platform.png');
+        this.load.image('backgroundSpace', 'assets/backgroundSpace.png');
+        this.load.image('asteroidBackground2', 'assets/asteroidsFar.png');
+        this.load.image('asteroidBackground3', 'assets/asteroidsNear.png');
+        //SpriteSheets
+        this.load.spritesheet("player", "assets/PlayerSheet.png",
+            { frameWidth: 512, frameHeight: 512 });
+        this.load.spritesheet("enemy_hovercraft", "assets/hovercraft_spritesheet.png",
+            { frameWidth: 512, frameHeight: 256 });
+        this.load.spritesheet("gun", "assets/gun.png",
+            { frameWidth: 1024, frameHeight: 256 });
+        this.load.spritesheet("sniper", "assets/sniper.png",
+            { frameWidth: 1024, frameHeight: 256 });
+        this.load.spritesheet("mortar", "assets/mortar.png",
+            { frameWidth: 1024, frameHeight: 256 });
+        this.load.spritesheet('mortar_orb', 'assets/mortar_bullet.png',
+            { frameWidth: 256, frameHeight: 256 });
+
+        //Audio
+        this.load.audio('fleet', 'assets/sound/SS_Fleet.mp3');
+        this.load.audio('city', 'assets/sound/SS_City.mp3');
+        this.load.audio('main', 'assets/sound/SS_Main.mp3');
+        this.load.audio('lost', 'assets/sound/SS_Lost.mp3');
+        this.load.audio('moon', 'assets/sound/SS_Moon.mp3');
+        this.load.audio('zone', 'assets/sound/SS_Zone.mp3');
+        this.load.audio('menu', 'assets/sound/Menu.mp3');;
+        
+        this.load.audio('shoot', 'assets/sound/LaserShoot.mp3');
+        this.load.audio('sound_rifle', 'assets/sound/sound_rifle.mp3');
+        this.load.audio('sound_sniper', 'assets/sound/sound_sniper.mp3');
+        this.load.audio('sound_mortar', 'assets/sound/sound_mortar.mp3');
+        this.load.audio('sound_explosion', 'assets/sound/sound_explosion.mp3');
+        this.load.audio('empty_gun', 'assets/sound/GunEmpty.mp3')
+
+
+        //Maps & levels
+        this.load.image("tileset_image", "assets/Tileset_game.png");
+        this.load.tilemapTiledJSON("Beta_test", "levels/Beta_test.json");
+        this.load.tilemapTiledJSON("Mission01_scene01", "levels/Mission01_scene01.json");
+        this.load.tilemapTiledJSON("Mission01_scene02", "levels/Mission01_scene02.json");
+        this.load.tilemapTiledJSON("Mission01_scene03", "levels/Mission01_scene03.json");
+        this.load.tilemapTiledJSON("Mission01_scene04", "levels/Mission01_scene04.json");
+        this.load.tilemapTiledJSON("Mission01_scene05", "levels/Mission01_scene05.json");
+        this.load.tilemapTiledJSON("Mission01_scene06", "levels/Mission01_scene06.json");
+
+    }
+
+    create() {
+        const frames = 30
+        const musicVolume = 0.35
+        const fxVolume = 0.35
+        this.sound.play("zone", { volume: musicVolume, loop: true });
+        this.anims.create({
+            key: "player_idle_right",
+            frames: this.anims.generateFrameNumbers("player", { start: frames * 0, end: frames * 1 - 1 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "player_run_right",
+            frames: this.anims.generateFrameNumbers("player", { start: frames * 1, end: frames * 2 - 1 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "player_jump_right",
+            frames: this.anims.generateFrameNumbers("player", { start: frames * 2, end: frames * 3 - 1 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "player_fall_right",
+            frames: this.anims.generateFrameNumbers("player", { start: frames * 3, end: frames * 4 - 1 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "player_backwards_right",
+            frames: this.anims.generateFrameNumbers("player", { start: frames * 5, end: frames * 6 - 1 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "rifle_effects",
+            frames: this.anims.generateFrameNumbers("gun", { start: 0, end: 36 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "sniper_effects",
+            frames: this.anims.generateFrameNumbers("sniper", { start: 0, end: 36 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "mortar_effects",
+            frames: this.anims.generateFrameNumbers("mortar", { start: 0, end: 36 }),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key: "mortar_orb_effects",
+            frames: this.anims.generateFrameNumbers("mortar_orb", { start: 0, end: 36 }),
+            frameRate: 36,
+        });
+
+        this.scene.start("MainMenu", {
+            musicVolume: musicVolume,
+            fxVolume: fxVolume
+        });
+    }
+}
+export default Preload
